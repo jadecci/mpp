Add effective connectivity features computation. Fix bugs

mpp/interfaces/crossval.py
@@ -129,7 +129,7 @@ class RegionwiseModel(SimpleInterface):
         l1_ratios = np.zeros(train_x.shape[2])
         for region in range(train_x.shape[2]):
             if self.inputs.selected[f'regions_level{self.inputs.level}'][region]:
-                r[region], cod[region], coef[region, :], l1_ratios[region, :] = elastic_net(
+                r[region], cod[region], coef[region, :], l1_ratios[region] = elastic_net(
                     train_x[:, :, region], train_y, test_x[:, :, region], test_y,
                     int(self.inputs.config['n_alphas']))
 

mpp/interfaces/features.py
@@ -11,7 +11,8 @@ import pandas as pd
 from nipype.interfaces import fsl, freesurfer
 import bct
 
-from mpp.utilities.features import fc, diffusion_mapping, score, add_subdir, atlas_files, add_annot
+from mpp.utilities.features import (
+    parcellate, fc, diffusion_mapping, score, add_subdir, atlas_files, add_annot)
 from mpp.utilities.data import read_h5
 from mpp.utilities.preproc import t1_files_type, fs_files_aparc, combine_4strings
 
@@ -29,6 +30,7 @@ class _RSFCInputSpec(BaseInterfaceInputSpec):
 class _RSFCOutputSpec(TraitedSpec):
     rsfc = traits.Dict(dtype=float, desc='resting-state functional connectivity')
     dfc = traits.Dict(dtype=float, desc='dynamic functional connectivity')
+    efc = traits.Dict(dtype=float, desc='effective functional connectivity')
 
 
 class RSFC(SimpleInterface):
@@ -37,9 +39,8 @@ class RSFC(SimpleInterface):
     output_spec = _RSFCOutputSpec
 
     def _run_interface(self, runtime):
-        self._results['rsfc'] = {}
-        self._results['dfc'] = {}
         n_runs = len(self.inputs.rs_runs) + self.inputs.hcpd_b_runs
+        tavg_dict = {}
         for i in range(n_runs):
             if self.inputs.dataset == 'HCP-D' and i >= 4:
                 run = self.inputs.rs_runs[i-3]
@@ -53,9 +54,14 @@ class RSFC(SimpleInterface):
             if self.inputs.rs_files[key_surf] and self.inputs.rs_files[key_vol]:
                 t_surf = nib.load(self.inputs.rs_files[key_surf]).get_fdata()
                 t_vol = nib.load(self.inputs.rs_files[key_vol]).get_fdata()
-                self._results['rsfc'], self._results['dfc'] = fc(
-                    t_surf, t_vol, self.inputs.dataset, self.inputs.rs_files,
-                    self._results['rsfc'], self._results['dfc'])
+                tavg = parcellate(t_surf, t_vol, self.inputs.dataset, self.inputs.rs_files)
+                for key, val in tavg.items():
+                    if tavg_dict[key].size:
+                        tavg_dict[key] = np.hstack((tavg_dict[key], val))
+                    else:
+                        tavg_dict[key] = val
+
+        self._results['rsfc'], self._results['dfc'], self._results['efc'] = fc(tavg_dict)
 
         return runtime
 
@@ -114,8 +120,8 @@ class TFC(SimpleInterface):
             if self.inputs.t_files[f'{run}_surf'] and self.inputs.t_files[f'{run}_vol']:
                 t_surf = nib.load(self.inputs.t_files[f'{run}_surf']).get_fdata()
                 t_vol = nib.load(self.inputs.t_files[f'{run}_vol']).get_fdata()
-                self._results['tfc'], _ = fc(
-                    t_surf, t_vol, self.inputs.dataset, self.inputs.t_files, self._results['tfc'])
+                tavg = parcellate(t_surf, t_vol, self.inputs.dataset, self.inputs.t_files)
+                self._results['tfc'][run], _, _ = fc(tavg)
 
         return runtime
 

mpp/utilities/features.py
@@ -11,6 +11,7 @@ from scipy.ndimage import binary_erosion
 from sklearn.metrics import pairwise_distances
 from mapalign.embed import compute_diffusion_map
 from statsmodels.formula.api import ols
+from rdcmpy import RegressionDCM
 
 from mpp.exceptions import DatasetError
 
@@ -18,9 +19,8 @@ base_dir = Path(__file__).resolve().parent.parent
 logging.getLogger('datalad').setLevel(logging.WARNING)
 
 
-def fc(
-        t_surf: np.ndarray, t_vol: np.ndarray, dataset: str, func_files: dict, sfc_dict: dict,
-        dfc_dict: Union[dict, None] = None) -> tuple[dict, Union[dict, None]]:
+def parcellate(
+        t_surf: np.ndarray, t_vol: np.ndarray, dataset: str, func_files: dict) -> dict:
     if dataset in ['HCP-YA', 'HCP-A', 'HCP-D']:
         conf = nuisance_conf_hcp(t_vol, func_files['atlas_mask'])
     else:
@@ -31,6 +31,7 @@ def fc(
         np.linspace(-1, 1, num=conf.shape[0]).reshape((conf.shape[0], 1))), axis=1)
     t_surf_resid = t_surf - np.dot(regressors, np.linalg.lstsq(regressors, t_surf, rcond=-1)[0])
 
+    tavg_dict = {}
     for level in range(4):
         key = f'level{level+1}'
         parc_sch_file, _, _, parc_mel_file, _ = atlas_files(Path(base_dir, 'data'), level)
@@ -60,32 +61,43 @@ def fc(
                        :, np.where(np.abs(selected.mean(axis=0)) >= sys.float_info.epsilon)[0]]
             parc_vol[parcel-1, :] = selected.mean(axis=1)
 
-        tavg = np.concatenate([parc_surf, parc_vol], axis=0)
+        tavg_dict[key] = np.vstack((parc_surf, parc_vol))
+
+    return tavg_dict
+
+
+def fc(tavg_dict: dict, t_rep: Union[float, None] = None) -> tuple[dict, dict, dict]:
+    sfc_dict = {}
+    dfc_dict = {}
+    efc_dict = {}
+    for level in range(4):
+        key = f'level{level+1}'
+        tavg = tavg_dict[key]
 
         # static FC (Fisher's z excluding diagonals)
-        sfc = np.corrcoef(tavg)
+        sfc = np.corrcoef(tavg_dict[key])
         sfc = (
                 0.5 * (np.log(1 + sfc, where=~np.eye(sfc.shape[0], dtype=bool)) -
                        np.log(1 - sfc, where=~np.eye(sfc.shape[0], dtype=bool))))
         sfc[np.diag_indices_from(sfc)] = 0
-        if sfc_dict[key].size:
-            sfc_dict[key] = np.dstack((sfc_dict[key], sfc))
-        else:
-            sfc_dict[key] = sfc
+        sfc_dict[key] = sfc
 
-        # dynamic FC (optional): 1st order ARR model
+        # dynamic FC: 1st order ARR model
         # see https://github.com/ThomasYeoLab/CBIG/blob/master/stable_projects/fMRI_dynamics/Liegeois2017_Surrogates/CBIG_RL2017_ar_mls.m # noqa: E501
-        if dfc_dict is not None:
-            y = tavg[:, range(1, tavg.shape[1])]
-            z = np.ones((tavg.shape[0]+1, tavg.shape[1]-1))
-            z[1:(tavg.shape[0]+1), :] = tavg[:, range(tavg.shape[1]-1)]
-            b = np.linalg.lstsq((z @ z.T).T, (y @ z.T).T, rcond=None)[0].T
-            if dfc_dict[key].size:
-                dfc_dict[key] = np.dstack((dfc_dict[key], b[:, range(1, b.shape[1])]))
-            else:
-                dfc_dict[key] = b[:, range(1, b.shape[1])]
+        y = tavg[:, range(1, tavg.shape[1])]
+        z = np.ones((tavg.shape[0] + 1, tavg.shape[1] - 1))
+        z[1:(tavg.shape[0] + 1), :] = tavg[:, range(tavg.shape[1] - 1)]
+        b = np.linalg.lstsq((z @ z.T).T, (y @ z.T).T, rcond=None)[0].T
+        dfc_dict[key] = b[:, range(1, b.shape[1])]
 
-    return sfc_dict, dfc_dict
+        # effective FC: regression DCM
+        if t_rep is not None:
+            rdcm = RegressionDCM(data=tavg.T, t_rep=t_rep)
+            rdcm.estimate()
+            params = rdcm.get_params()
+            efc_dict[key] = params['mu_connectivity']
+
+    return sfc_dict, dfc_dict, efc_dict
 
 
 def nuisance_conf_hcp(t_vol: np.ndarray, atlas_file: Union[Path, str]) -> np.ndarray: