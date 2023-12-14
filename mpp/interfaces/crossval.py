06d4047bce8558134019282e549f462e459eaa24
Wu Jianxiao <jianxiao.wu.veronica@gmail.com>
2023-12-14 15:05:07 +0100

    Remove unused code and simplify code

--- mpp/interfaces/crossval.py
+++ mpp/interfaces/crossval.py
@@ -1,15 +1,11 @@
 import itertools
-from pathlib import Path
 
 from nipype.interfaces.base import BaseInterfaceInputSpec, TraitedSpec, SimpleInterface, traits
 import numpy as np
 from sklearn.model_selection import RepeatedStratifiedKFold, KFold
-from statsmodels.stats.multitest import multipletests
 
-from mpp.utilities.models import (
-    elastic_net, kernel_ridge_corr_cv, linear, random_forest_cv, lasso_cv, random_patches,
-    permutation_test)
-from mpp.utilities.data import write_h5, cv_extract_subject_data, cv_extract_all_features
+from mpp.utilities.models import elastic_net, random_forest_cv
+from mpp.utilities.data import cv_extract_subject_data
 from mpp.utilities.features import pheno_reg_conf
 
 
@@ -44,190 +40,6 @@ class CrossValSplit(SimpleInterface):
         for fold, (_, test_ind) in enumerate(rskf.split(subjects, datasets)):
             key = f'repeat{int(np.floor(fold/n_folds))}_fold{int(fold%n_folds)}'
             self._results['cv_split'][key] = np.array(subjects)[test_ind]
-
-        return runtime
-
-
-class _RegionwiseModelInputSpec(BaseInterfaceInputSpec):
-    sublists = traits.Dict(mandatory=True, dtype=list, desc='available subjects in each dataset')
-    features_dir = traits.Dict(
-        mandatory=True, dtype=str, desc='absolute path to extracted features for each dataset')
-    phenotypes = traits.Dict(mandatory=True, dtype=float, desc='phenotype values')
-    phenotypes_perm = traits.Dict({}, dtype=float, desc='shuffled phenotype values for permutation')
-
-    embeddings = traits.Dict(mandatory=True, desc='embeddings for gradients')
-    params = traits.Dict(mandatory=True, desc='parameters for anatomical connectivity')
-
-    cv_split = traits.Dict(mandatory=True, dtype=list, desc='test subjects of each fold')
-    cv_split_perm = traits.Dict({}, dtype=list, desc='test subjects of each fold')
-
-    level = traits.Str(mandatory=True, desc='parcellation level (1 to 4)')
-    repeat = traits.Int(mandatory=True, desc='current repeat of cross-validation')
-    fold = traits.Int(mandatory=True, desc='current fold in the repeat')
-
-    mode = traits.Str(
-        mandatory=True, desc=("'validate' to train and validate models with permutation tests, "
-                              "'test' to train and test models on test folds"))
-    selected = traits.Dict(dtype=list, desc='selected regions for each parcellation level')
-    config = traits.Dict(mandatory=True, desc='configuration settings')
-
-
-class _RegionwiseModelOutputSpec(TraitedSpec):
-    results = traits.Dict(desc='accuracy results')
-    rw_ypred = traits.Dict(desc='Predicted psychometric values')
-
-
-class RegionwiseModel(SimpleInterface):
-    """Train and validate, or test, a region-wise model"""
-    input_spec = _RegionwiseModelInputSpec
-    output_spec = _RegionwiseModelOutputSpec
-
-    def _extract_data(
-            self, subjects: list, repeat: int, phenotypes: dict,
-            permutation: bool = False) -> tuple[np.ndarray, ...]:
-        x_all, y = cv_extract_all_features(
-            subjects, self.inputs.sublists, self.inputs.features_dir, self.inputs.level,
-            self.inputs.embeddings, self.inputs.params, repeat, phenotypes, permutation=permutation)
-
-        return x_all, y
-
-    def _validate(self, repeat: int, permutation: bool = False) -> tuple[np.ndarray, ...]:
-        if permutation:
-            cv_split = self.inputs.cv_split_perm
-            phenotypes = self.inputs.phenotypes_perm
-        else:
-            cv_split = self.inputs.cv_split
-            phenotypes = self.inputs.phenotypes
-        n_folds = int(self.inputs.config["n_folds"])
-        all_sub = sum(self.inputs.sublists.values(), [])
-
-        test_sub = cv_split[f'repeat{repeat}_fold{self.inputs.fold}']
-        val_sub = cv_split[f'repeat{repeat}_fold{(self.inputs.fold+1)%n_folds}']
-        testval_sub = np.concatenate((val_sub, test_sub))
-        train_sub = [subject for subject in all_sub if subject not in testval_sub]
-
-        train_x, train_y = self._extract_data(train_sub, repeat, phenotypes, permutation)
-        val_x, val_y = self._extract_data(val_sub, repeat, phenotypes, permutation)
-
-        r = np.zeros(train_x.shape[2])
-        cod = np.zeros(train_x.shape[2])
-        for region in range(train_x.shape[2]):
-            r[region], cod[region], _, _ = elastic_net(
-                train_x[:, :, region], train_y, val_x[:, :, region], val_y,
-                int(self.inputs.config['n_alphas']))
-        r = np.nan_to_num(r)
-
-        return r, cod
-
-    def _test(self) -> None:
-        all_sub = sum(self.inputs.sublists.values(), [])
-        test_sub = self.inputs.cv_split[f'repeat{self.inputs.repeat}_fold{self.inputs.fold}']
-        train_sub = [subject for subject in all_sub if subject not in test_sub]
-
-        train_x, train_y = self._extract_data(train_sub, self.inputs.repeat, self.inputs.phenotypes)
-        test_x, test_y = self._extract_data(test_sub, self.inputs.repeat, self.inputs.phenotypes)
-
-        r = np.zeros(train_x.shape[2])
-        cod = np.zeros(train_x.shape[2])
-        coef = np.zeros((train_x.shape[2], train_x.shape[1]+1))
-        train_ypred = np.zeros((len(train_y), train_x.shape[2]))
-        test_ypred = np.zeros((len(test_y), train_x.shape[2]))
-        for region in range(train_x.shape[2]):
-            # if self.inputs.selected[f'regions_level{self.inputs.level}'][region]:
-            r[region], cod[region], train_ypred[:, region], test_ypred[:, region] = elastic_net(
-                train_x[:, :, region], train_y, test_x[:, :, region], test_y,
-                int(self.inputs.config['n_alphas']))
-
-        key = f'repeat{self.inputs.repeat}_fold{self.inputs.fold}_level{self.inputs.level}'
-        self._results['results'][f'r_{key}'] = r
-        self._results['results'][f'cod_{key}'] = cod
-        self._results['results'][f'model_{key}'] = coef
-        self._results['rw_ypred'] = {'train_ypred': train_ypred, 'test_ypred': test_ypred}
-
-    def _run_interface(self, runtime):
-        self._results['results'] = {}
-
-        if self.inputs.mode == 'validate':
-            r, cod = self._validate(self.inputs.repeat)
-            key = f'repeat{self.inputs.repeat}_fold{self.inputs.fold}_level{self.inputs.level}'
-            self._results['results'][f'r_{key}'] = r
-            self._results['results'][f'cod_{key}'] = cod
-
-            # assuming n_repeats_perm = n_repeats x 10
-            n_perm_check = int(self.inputs.config['n_repeats']) * 10
-            if int(self.inputs.config['n_repeats_perm']) == n_perm_check:
-                for repeat_split in range(10):
-                    repeat = int(self.inputs.repeat) * 10 + repeat_split
-                    key = f'repeat{repeat}_fold{self.inputs.fold}_level{self.inputs.level}'
-                    r, cod = self._validate(repeat, permutation=True)
-                    self._results['results'][f'r_perm_{key}'] = r
-                    self._results['results'][f'cod_perm_{key}'] = cod
-            else:
-                raise ValueError("'n_repeats_perm' must be 10 x 'n_repeats'")
-
-        elif self.inputs.mode == 'test':
-            self._test()
-
-        return runtime
-
-
-class _RegionSelectInputSpec(BaseInterfaceInputSpec):
-    results = traits.List(mandatory=True, dtype=dict, desc='accuracy results')
-    levels = traits.List(mandatory=True, desc='parcellation levels (1 to 4)')
-    output_dir = traits.Directory(mandatory=True, desc='absolute path to output directory')
-    overwrite = traits.Bool(mandatory=True, desc='whether to overwrite existing results')
-    config = traits.Dict(mandatory=True, desc='configuration settings')
-    phenotype = traits.Str(mandatory=True, desc='phenotype to use as prediction target')
-
-
-class _RegionSelectOutputSpec(TraitedSpec):
-    selected = traits.Dict(dtype=list, desc='selected regions for each parcellation level')
-
-
-class RegionSelect(SimpleInterface):
-    """Select region-wise models that pass permutation tests"""
-    input_spec = _RegionSelectInputSpec
-    output_spec = _RegionSelectOutputSpec
-
-    def _extract_results(
-            self, results_dict: dict, level: str,  permutation: bool = False) -> np.ndarray:
-        if permutation:
-            n_repeats = self.inputs.config['n_repeats_perm']
-            key = f'{self.inputs.config["features_criteria"]}_perm'
-        else:
-            n_repeats = self.inputs.config['n_repeats']
-            key = self.inputs.config['features_criteria']
-        n_folds = self.inputs.config['n_folds']
-
-        n_region_dict = {'1': 116, '2': 232, '3': 350, '4': 454}
-
-        results = np.zeros((int(n_repeats), n_region_dict[level]))
-        for repeat in range(int(n_repeats)):
-            for fold in range(int(n_folds)):
-                key_curr = f'{key}_repeat{repeat}_fold{fold}_level{level}'
-                results[repeat, :] = results[repeat, :] + results_dict[key_curr]
-            results[repeat, :] = np.divide(results[repeat, :], int(n_folds))
-
-        return results
-
-    def _save_results(self) -> None:
-        Path(self.inputs.output_dir).mkdir(parents=True, exist_ok=True)
-        output_file = Path(self.inputs.output_dir, f'regionwise_results_{self.inputs.phenotype}.h5')
-        for key, val in self._results['selected'].items():
-            write_h5(output_file, f'/{key}', np.array(val), self.inputs.overwrite)
-
-    def _run_interface(self, runtime):
-        results_dict = {key: item for d in self.inputs.results for key, item in d.items()}
-
-        self._results['selected'] = {}
-        for level in self.inputs.levels:
-            results = self._extract_results(results_dict, level)
-            results_perm = self._extract_results(results_dict, level, permutation=True)
-
-            p = permutation_test(results.mean(axis=0), results_perm)
-            selected = multipletests(p, method='fdr_bh')[0]
-            self._results['selected'][f'regions_level{level}'] = selected
-        self._save_results()
 
         return runtime
 
@@ -309,10 +121,6 @@ class FeaturewiseModel(SimpleInterface):
             train_x, train_y, test_x, test_y, int(self.inputs.config['n_alphas']))
         self._results['results'][f'en_r_{key}'] = r
         self._results['results'][f'en_cod_{key}'] = cod
-
-        # r, cod, _, _ = kernel_ridge_corr_cv(train_x, train_y, test_x, test_y)
-        # self._results['results'][f'krcorr_r_{key}'] = r
-        # self._results['results'][f'krcorr_cod_{key}'] = cod
 
         return train_ypred, test_ypred, cod_train
 
@@ -410,15 +218,6 @@ class ConfoundsModel(SimpleInterface):
                     'test_ypred': np.vstack((ypred['test_ypred'], test_pred)),
                     'train_cod': np.concatenate((ypred['train_cod'], [cod]))}
 
-        # Grouped-confound models
-        # conf_group = {'demo': [0, 1, 5, 6, 7], 'brain': [3, 4], 'qn': [0, 1, 2]}
-        # for group, inds in conf_group.items():
-        #     train_pred, test_pred = self._test(
-        #         train_x[:, inds], train_y, test_x[:, inds], test_y, group)
-        #     ypred = {
-        #         'train_ypred': np.vstack((ypred['train_ypred'], train_pred)),
-        #         'test_ypred': np.vstack((ypred['test_ypred'], test_pred))}
-
         # All-confound models
         train_pred, test_pred, cod = self._test(train_x, train_y, test_x, test_y, 'all')
         ypred = {
@@ -485,52 +284,5 @@ class IntegratedFeaturesModel(SimpleInterface):
             self._rfr(
                 train_y, test_y, train_ypred[:, f_ranks[:n_feature]],
                 test_ypred[:, f_ranks[:n_feature]], key)
-
-        return runtime
-
-
-class _RandomPatchesModelInputSpec(BaseInterfaceInputSpec):
-    sublists = traits.Dict(mandatory=True, dtype=list, desc='available subjects in each dataset')
-    features_dir = traits.Dict(
-        mandatory=True, dtype=str, desc='absolute path to extracted features for each dataset')
-    phenotypes = traits.Dict(mandatory=True, dtype=float, desc='phenotype values')
-    embeddings = traits.Dict(mandatory=True, desc='embeddings for gradients')
-    params = traits.Dict(mandatory=True, desc='parameters for anatomical connectivity')
-    cv_split = traits.Dict(mandatory=True, dtype=list, desc='test subjects of each fold')
-
-    level = traits.Str(mandatory=True, desc='parcellation level (1 to 4)')
-    repeat = traits.Int(mandatory=True, desc='current repeat of cross-validation')
-    fold = traits.Int(mandatory=True, desc='current fold in the repeat')
-
-
-class _RandomPatchesModelOutputSpec(TraitedSpec):
-    results = traits.Dict(desc='accuracy results')
-
-
-class RandomPatchesModel(SimpleInterface):
-    """Train and test the integrated features model"""
-    input_spec = _RandomPatchesModelInputSpec
-    output_spec = _RandomPatchesModelOutputSpec
-
-    def _extract_data(self, subjects: list) -> tuple[np.ndarray, np.ndarray,]:
-        x_all, y = cv_extract_all_features(
-            subjects, self.inputs.sublists, self.inputs.features_dir, self.inputs.level,
-            self.inputs.embeddings, self.inputs.params, self.inputs.repeat, self.inputs.phenotypes)
-
-        return x_all, y
-
-    def _run_interface(self, runtime):
-        self._results['results'] = {}
-        all_sub = sum(self.inputs.sublists.values(), [])
-        test_sub = self.inputs.cv_split[f'repeat{self.inputs.repeat}_fold{self.inputs.fold}']
-        train_sub = [subject for subject in all_sub if subject not in test_sub]
-
-        train_x, train_y = self._extract_data(train_sub)
-        test_x, test_y = self._extract_data(test_sub)
-
-        key = f'repeat{self.inputs.repeat}_fold{self.inputs.fold}_level{self.inputs.level}'
-        r, cod = random_patches(train_x, train_y, test_x, test_y)
-        self._results['results'][f'rp_r_{key}'] = r
-        self._results['results'][f'rp_cod_{key}'] = cod
 
         return runtime
