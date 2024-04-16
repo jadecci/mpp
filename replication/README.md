1dbe1c2644e8a340b164d0b257d827fff4fb2e9c
Wu Jianxiao <jianxiao.wu.veronica@gmail.com>
2024-04-16 13:37:30 +0200

    Add an exclusion list for HCP-D subjects

--- replication/README.md
+++ replication/README.md
@@ -21,7 +21,8 @@ ${project_dir}/phenotype
 ```bash
 python3 ${project_dir}/mpp/replication/sublist/create_allRun_sublists.py \
     ${project_dir}/phenotype ${project_dir}/sublist \
-    ${project_dir}/mpp/replication/sublist/HCP-A_exclude.csv
+    ${project_dir}/mpp/replication/sublist/HCP-A_exclude.csv \
+    ${project_dir}/mpp/replication/sublist/HCP-D_exclude.csv
 ```
 2. Preprocess diffusion data for HCP Aging and HCP Development
 ```bash
--- replication/sublist/create_allRun_sublists.py
+++ replication/sublist/create_allRun_sublists.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument("pheno_dir", type=Path, help="absolute path to phenotype directory")
 parser.add_argument("out_dir", type=Path, help="absolute path to output directory")
 parser.add_argument("hcpa_exclude", type=Path, help="absolute path to HCP-A exclusion list")
+parser.add_argument("hcpd_exclude", type=Path, help="absolute path to HCP-D exclusion list")
 args = parser.parse_args()
 
 # HCP-YA
@@ -36,5 +37,7 @@ hcpd_pheno_file = Path(args.pheno_dir, "HCP-D", "ndar_subject01.txt")
 hcpd_data = pd.read_table(
     hcpd_pheno_file, sep="\t", header=0, skiprows=[1], usecols=["src_subject_id"])
 hcpd_data["src_subject_id"] = hcpd_data["src_subject_id"].str.cat(["_V1_MR"]*len(hcpd_data))
+hcpd_exclude = pd.read_csv(args.hcpd_exclude, header=None).squeeze().to_list()
+hcpd_data.drop(hcpd_data.loc[hcpd_data["src_subject_id"].isin(hcpd_exclude)].index, inplace=True)
 hcpd_out_file = Path(args.out_dir, "HCP-D_allRun.csv")
 hcpd_data.to_csv(hcpd_out_file, header=False, index=False)
