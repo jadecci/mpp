8479bc4eb7d9d51c00829c8654bb7867ed3c682e
Wu Jianxiao <jianxiao.wu.veronica@gmail.com>
2026-09-03 11:08:50 +0200

    Update fusion CCA scripts to run separately for each dataset instead.

--- replication/analysis/fusion_cca_analysis/collect_data_fusion.py
+++ replication/analysis/fusion_cca_analysis/collect_data_fusion.py
@@ -24,6 +24,7 @@ task_feature_list = {
 parser = argparse.ArgumentParser(
     description="Collect data for fusion CCA analysis",
     formatter_class=lambda prog: argparse.ArgumentDefaultsHelpFormatter(prog, width=100))
+parser.add_argument("--dataset", type=str, dest="dataset", required=True, help="Dataset")
 parser.add_argument(
     "--sublist_dir", type=Path, dest="sublist_dir", required=True, help="Sublist directory")
 parser.add_argument(
@@ -32,58 +33,57 @@ parser.add_argument(
     "--work_dir", type=Path, dest="work_dir", required=True, help="Absolute path to work directory")
 args = parser.parse_args()
 
-args.out_dir.mkdir(parents=True, exist_ok=True)
-level = 3
-
 # Install dataset with multimodal features collected for prediction
 mfe_url = "git@gin.g-node.org:/jadecci/multimodal_features.git"
-root_mfe_dir = Path(args.work_dir, "mfe_features")
+root_mfe_dir = Path(args.work_dir, f"{args.dataset}_mfe_features")
 dl.install(root_mfe_dir, source=mfe_url)
 
-# Extract and save data separately for each dataset
-for dataset in ["HCP-D", "HCP-YA", "HCP-A"]:
-    features = base_feature_list + task_feature_list[dataset]
-    sublist = pd.read_table(
-        Path(args.sublist_dir, f"{dataset}_allRun.csv"), header=None, dtype=str).squeeze("columns")
+# Set-up
+args.out_dir.mkdir(parents=True, exist_ok=True)
+level = 3
+features = base_feature_list + task_feature_list[args.dataset]
+sublist = pd.read_table(
+    Path(args.sublist_dir, f"{args.dataset}_allRun.csv"), header=None, dtype=str).squeeze("columns")
 
-    # DTI feature file
-    dti_file = Path(root_mfe_dir, f"{dataset}_dti.h5")
-    dl.get(dti_file, dataset=root_mfe_dir)
+# DTI feature file
+dti_file = Path(root_mfe_dir, f"{args.dataset}_dti.h5")
+dl.get(dti_file, dataset=root_mfe_dir)
 
-    # Iterate through subjects in the dataset
-    y = []
-    conf = []
-    x = []
-    for subject in sublist:
-        sub_file = Path(root_mfe_dir, dataset, f"{subject}.h5")
-        dl.get(sub_file, dataset=root_mfe_dir)
+# Iterate through subjects in the dataset
+y = []
+conf = []
+x = []
+for i, subject in enumerate(sublist):
+    sub_file = Path(root_mfe_dir, args.dataset, f"{subject}.h5")
+    dl.get(sub_file, dataset=root_mfe_dir)
 
-        # Get phenotypes and confounds
-        y.append(pd.DataFrame(pd.read_hdf(sub_file, "phenotype")))
-        conf.append(pd.DataFrame(pd.read_hdf(sub_file, "confound")))
+    # Get phenotypes and confounds
+    y.append(pd.DataFrame(pd.read_hdf(sub_file, "phenotype")))
+    conf.append(pd.DataFrame(pd.read_hdf(sub_file, "confound")))
 
-        # Iterate through feature types
-        x_sub = []
-        for feature in features:
-            if feature == "rs_stats":
-                x_curr = pd.DataFrame(pd.read_hdf(sub_file, "rs_par_level3"))
-            elif feature in ["d_fa", "d_md", "d_ad", "d_rd"]:
-                feature_name = feature.split("d_")[1]
-                x_curr = pd.DataFrame(pd.read_hdf(dti_file, f"{feature_name}_{subject}")).T
-            else:
-                x_curr = pd.DataFrame(pd.read_hdf(sub_file, f"{feature}_level3"))
-            x_curr = x_curr.replace(-np.inf, 0)
-            x_curr = x_curr.fillna(value=0)
-            x_curr.columns = [f"{feature}_{col}" for col in range(len(x_curr.columns))]
-            x_sub.append(x_curr)
-        x.append(pd.concat(x_sub, axis="columns"))
+    # Iterate through feature types
+    x_sub = []
+    for feature in features:
+        if feature == "rs_stats":
+            x_curr = pd.DataFrame(pd.read_hdf(sub_file, "rs_par_level3"))
+        elif feature in ["d_fa", "d_md", "d_ad", "d_rd"]:
+            feature_name = feature.split("d_")[1]
+            x_curr = pd.DataFrame(pd.read_hdf(dti_file, f"{feature_name}_{subject}")).T
+        else:
+            x_curr = pd.DataFrame(pd.read_hdf(sub_file, f"{feature}_level3"))
+        x_curr = x_curr.replace(-np.inf, 0)
+        x_curr = x_curr.fillna(value=0)
+        x_curr.columns = [f"{feature}_{col}" for col in range(len(x_curr.columns))]
+        x_sub.append(x_curr)
+    x.append(pd.concat(x_sub, axis="columns"))
 
-        dl.drop(sub_file, dataset=root_mfe_dir)
-    dl.drop(dti_file, dataset=root_mfe_dir)
+    print(f"Extracted data for {args.dataset }subject {i}: {subject}")
+    dl.drop(sub_file, dataset=root_mfe_dir)
+dl.drop(dti_file, dataset=root_mfe_dir)
 
-    # Save data
-    pd.concat(y, axis="index").to_csv(Path(args.out_dir, f"{dataset}_y.csv"))
-    pd.concat(conf, axis="index").to_csv(Path(args.out_dir, f"{dataset}_conf.csv"))
-    pd.concat(x, axis="index").to_csv(Path(args.out_dir, f"{dataset}_x.csv"))
+# Save data
+pd.concat(y, axis="index").to_csv(Path(args.out_dir, f"{args.dataset}_y.csv"))
+pd.concat(conf, axis="index").to_csv(Path(args.out_dir, f"{args.dataset}_conf.csv"))
+pd.concat(x, axis="index").to_csv(Path(args.out_dir, f"{args.dataset}_x.csv"))
 
 dl.remove(dataset=root_mfe_dir, reckless="kill")
--- replication/analysis/fusion_cca_analysis/fusion_cca.py
+++ replication/analysis/fusion_cca_analysis/fusion_cca.py
@@ -10,6 +10,14 @@ import numpy as np
 import pandas as pd
 
 
+task_list = {
+    "HCP-YA": [
+        "tfMRI_EMOTION", "tfMRI_GAMBLING", "tfMRI_LANGUAGE", "tfMRI_MOTOR", "tfMRI_WM",
+        "tfMRI_RELATIONAL", "tfMRI_SOCIAL"],
+    "HCP-A": ["tfMRI_CARIT_PA", "tfMRI_FACENAME_PA", "tfMRI_VISMOTOR_PA"],
+    "HCP-D": ["tfMRI_CARIT", "tfMRI_EMOTION", "tfMRI_GUESSING"]}
+
+
 def apply_ac_params(morph: pd.DataFrame, param: pd.DataFrame) -> pd.DataFrame:
     ac = []
     for i in range(nparc):
@@ -40,6 +48,7 @@ def cca(
 parser = argparse.ArgumentParser(
     description="Run fusion CCA in cross-validation",
     formatter_class=lambda prog: argparse.ArgumentDefaultsHelpFormatter(prog, width=100))
+parser.add_argument("--dataset", type=str, dest="dataset", required=True, help="Dataset")
 parser.add_argument(
     "--data_dir", type=Path, dest="data_dir", required=True, help="Absolute path to collected data")
 parser.add_argument(
@@ -54,160 +63,151 @@ cv_seed = 42
 n_repeats = 10
 n_folds = 10
 
-# list of tasks in each dataset
-task_list = {
-    "HCP-YA": [
-        "tfMRI_EMOTION", "tfMRI_GAMBLING", "tfMRI_LANGUAGE", "tfMRI_MOTOR", "tfMRI_WM",
-        "tfMRI_RELATIONAL", "tfMRI_SOCIAL"],
-    "HCP-A": ["tfMRI_CARIT_PA", "tfMRI_FACENAME_PA", "tfMRI_VISMOTOR_PA"],
-    "HCP-D": ["tfMRI_CARIT", "tfMRI_EMOTION", "tfMRI_GUESSING"]}
-
 # Fixed feature types
 features_arr = ["rs_par", "s_myelin", "s_gmv"]
 features_surf = ["s_cs", "s_ct"]
 features_d = ["d_fa", "d_md", "d_ad", "d_rd"]
 
+# Dataset-specific feature types
+features_sym = ["rs_sfc"] + [f"{task}_sfc" for task in task_list[args.dataset]]
+features_asym = (
+        ["rs_dfc", "rs_ec", "d_scc", "d_scl"] + [f"{task}_ec" for task in task_list[args.dataset]])
+
+# Collected data for all subjects
+x = pd.read_csv(Path(args.data_dir, f"{args.dataset}_x.csv"), header=0, index_col=[0])
+y = pd.read_csv(Path(args.data_dir, f"{args.dataset}_y.csv"), header=0, index_col=[0])
+conf = pd.read_csv(Path(args.data_dir, f"{args.dataset}_conf.csv"), header=0, index_col=[0])
+subjects = x.index.to_list
+
+# Cross-validation splits
+if args.dataset == "HCP-YA":
+    fam_id = pd.read_csv(args.hcpya_res, usecols=["Subject", "Family_ID"])
+    fam_id = fam_id.loc[fam_id["Subject"].isin(subjects)]
+    rng = np.random.default_rng(seed=cv_seed)
+    cv_iter = [[[], []] for i in range(n_repeats * n_folds)]
+    fold_size_min = np.round(len(subjects) / n_folds)
+    for repeat in range(n_repeats):
+        ind_to_fill = np.arange(len(subjects))
+        for fold in range(n_folds):
+            cv_ind = fold + repeat * n_folds
+            n_max = len(subjects) - (fold + 1) * fold_size_min
+            while len(ind_to_fill) > n_max and len(ind_to_fill):
+                fill_start = rng.integers(low=0, high=len(ind_to_fill))
+                fill_start_ind = ind_to_fill[fill_start]
+                cv_iter[cv_ind][1].append(fill_start_ind)
+                ind_to_fill = np.delete(ind_to_fill, fill_start)
+
+                fill_fam_id = fam_id["Family_ID"].iloc[fill_start_ind]
+                fill_fam = fam_id["Subject"].loc[
+                    (fam_id["Family_ID"] == fill_fam_id) & (fam_id.index != fill_start_ind)]
+                for ind in fill_fam.index.to_list():
+                    cv_iter[cv_ind][1].append(ind)
+                    ind_to_fill = np.delete(ind_to_fill, np.where(ind_to_fill == ind))
+            cv_iter[cv_ind][0] = [
+                i for i in range(len(subjects)) if i not in cv_iter[cv_ind][1]]
+else:
+    rkf = RepeatedKFold(n_splits=n_folds, n_repeats=n_repeats, random_state=cv_seed)
+    cv_iter = rkf.split(subjects)
+
+# Iterate through folds
 results = []
-for dataset in ["HCP-D", "HCP-YA", "HCP-A"]:
-    # Dataset-specific feature types
-    features_sym = ["rs_sfc"] + [f"{task}_sfc" for task in task_list[dataset]]
-    features_asym = (
-            ["rs_dfc", "rs_ec", "d_scc", "d_scl"] + [f"{task}_ec" for task in task_list[dataset]])
+for fold, (train_ind, test_ind) in enumerate(cv_iter):
+    train_x = x.iloc[train_ind]
+    test_x = x.iloc[test_ind]
 
-    # Collected data for all subjects
-    x = pd.read_csv(Path(args.data_dir, f"{dataset}_x.csv"), header=0, index_col=[0])
-    y = pd.read_csv(Path(args.data_dir, f"{dataset}_y.csv"), header=0, index_col=[0])
-    conf = pd.read_csv(Path(args.data_dir, f"{dataset}_conf.csv"), header=0, index_col=[0])
-    subjects = x.index.to_list
+    # Add structural co-registration features
+    x_ac = {}
+    for feature in ["s_gmv", "s_cs", "s_ct"]:
+        # Get features from training set
+        nparc = 350 if feature == "s_gmv" else 300
+        features = train_x.filter(regex=f"{feature}_*", axis="columns")
+        features[features.columns] = features[features.columns].apply(pd.to_numeric)
+        features.columns = range(nparc)
+        features = features.join(pd.DataFrame({"mean": features.mean(axis=1)}))
 
-    # Cross-validation splits
-    if dataset == "HCP-YA":
-        fam_id = pd.read_csv(args.hcpya_res, usecols=["Subject", "Family_ID"])
-        fam_id = fam_id.loc[fam_id["Subject"].isin(subjects)]
-        rng = np.random.default_rng(seed=cv_seed)
-        cv_iter = [[[], []] for i in range(n_repeats * n_folds)]
-        fold_size_min = np.round(len(subjects) / n_folds)
-        for repeat in range(n_repeats):
-            ind_to_fill = np.arange(len(subjects))
-            for fold in range(n_folds):
-                cv_ind = fold + repeat * n_folds
-                n_max = len(subjects) - (fold + 1) * fold_size_min
-                while len(ind_to_fill) > n_max and len(ind_to_fill):
-                    fill_start = rng.integers(low=0, high=len(ind_to_fill))
-                    fill_start_ind = ind_to_fill[fill_start]
-                    cv_iter[cv_ind][1].append(fill_start_ind)
-                    ind_to_fill = np.delete(ind_to_fill, fill_start)
+        # Estimate paramaters
+        params = pd.DataFrame()
+        for i in range(nparc):
+            for j in range(nparc):
+                res = ols(f"features[{i}] ~ features[{j}] + mean", data=features).fit()
+                params[f"{i}_{j}"] = [
+                    res.params["Intercept"], res.params[f"features[{j}]"], res.params["mean"]]
 
-                    fill_fam_id = fam_id["Family_ID"].iloc[fill_start_ind]
-                    fill_fam = fam_id["Subject"].loc[
-                        (fam_id["Family_ID"] == fill_fam_id) & (fam_id.index != fill_start_ind)]
-                    for ind in fill_fam.index.to_list():
-                        cv_iter[cv_ind][1].append(ind)
-                        ind_to_fill = np.delete(ind_to_fill, np.where(ind_to_fill == ind))
-                cv_iter[cv_ind][0] = [
-                    i for i in range(len(subjects)) if i not in cv_iter[cv_ind][1]]
-    else:
-        rkf = RepeatedKFold(n_splits=n_folds, n_repeats=n_repeats, random_state=cv_seed)
-        cv_iter = rkf.split(subjects)
+        # Apply to all subjects
+        ac_feature = []
+        for subject in subjects:
+            features_sub = x.loc[subject].filter(regex=f"{feature}_*", axis="columns")
+            ac_curr = apply_ac_params(features_sub, params)
+            ac_curr.columns = [
+                f"s_ac{feature.split('s_')[1]}_{col}" for col in range(len(ac_curr.columns))]
+            ac_curr.index = [subject]
+            ac_feature.append(ac_curr)
+        x_ac[feature] = pd.concat(ac_feature, axis="index")
 
-    # Iterate through folds
-    for fold, (train_ind, test_ind) in enumerate(cv_iter):
-        train_x = x.iloc[train_ind]
-        test_x = x.iloc[test_ind]
+    # Iterate through prediction targets
+    for target, y_curr in y.items():
+        train_y, test_y = pheno_reg_conf(
+            y_curr.iloc[train_ind], conf.iloc[train_ind], y_curr.iloc[test_ind],
+            conf.iloc[test_ind])
 
-        # Add structural co-registration features
-        x_ac = {}
-        for feature in ["s_gmv", "s_cs", "s_ct"]:
-            # Get features from training set
-            nparc = 350 if feature == "s_gmv" else 300
-            features = train_x.filter(regex=f"{feature}_*", axis="columns")
-            features[features.columns] = features[features.columns].apply(pd.to_numeric)
-            features.columns = range(nparc)
-            features = features.join(pd.DataFrame({"mean": features.mean(axis=1)}))
-
-            # Estimate paramaters
-            params = pd.DataFrame()
-            for i in range(nparc):
-                for j in range(nparc):
-                    res = ols(f"features[{i}] ~ features[{j}] + mean", data=features).fit()
-                    params[f"{i}_{j}"] = [
-                        res.params["Intercept"], res.params[f"features[{j}]"], res.params["mean"]]
-
-            # Apply to all subjects
-            ac_feature = []
-            for subject in subjects:
-                features_sub = x.loc[subject].filter(regex=f"{feature}_*", axis="columns")
-                ac_curr = apply_ac_params(features_sub, params)
-                ac_curr.columns = [
-                    f"s_ac{feature.split('s_')[1]}_{col}" for col in range(len(ac_curr.columns))]
-                ac_curr.index = [subject]
-                ac_feature.append(ac_curr)
-            x_ac[feature] = pd.concat(ac_feature, axis="index")
-
-        # Iterate through prediction targets
-        for target, y_curr in y.items():
-            train_y, test_y = pheno_reg_conf(
-                y_curr.iloc[train_ind], conf.iloc[train_ind], y_curr.iloc[test_ind],
-                conf.iloc[test_ind])
-
-            # For connectivity features, iterate through edges
-            edge = 0
-            for i in range(350):
-                for j in range(i+1, 350):
-                    res_curr = {
-                        "Dataset": dataset, "Repeat": np.floor(fold / n_folds),
-                        "Fold": fold % n_folds, "Target": target}
-
-                    edge_opp = 350 * 350 - 1 - edge
-                    cols = (
-                        [f"{feature}_{edge}" for feature in (features_sym + features_asym)]
-                        + [f"{feature}_{edge_opp}" for feature in features_asym])
-                    cols_gmv = [f"s_acgmv_{edge}", f"s_acgmv_{edge_opp}"]
-                    train_x_edge = [train_x[cols], x_ac["s_gmv"][cols_gmv].iloc[train_ind]]
-                    test_x_edge = [test_x[cols], x_ac["s_gmv"][cols_gmv].iloc[test_ind]]
-
-                    # only include cortical features for edges between cortical regions
-                    if i < 300 and j < 300:
-                        edge_opp_surf = 300 * 300 - 1 - edge
-                        for feature in ["cs", "ct"]:
-                            cols = [f"s_ac{feature}_{edge}", f"s_ac{feature}_{edge_opp_surf}"]
-                            train_x_edge.append(x_ac[f"s_{feature}"][cols].iloc[train_ind])
-                            test_x_edge.append(x_ac[f"s_{feature}"][cols].iloc[test_ind])
-
-                    train_x_edge = pd.concat(train_x_edge, axis="columns")
-                    test_x_edge = pd.concat(test_x_edge, axis="columns")
-                    res_curr = res_curr | cca(train_x_edge, train_y, test_x_edge, test_y)
-
-                    results.append(pd.DataFrame(res_curr, index=[f"edge_{edge}"]))
-                    edge += 1
-
-            # For region-wise features, iterate through brain regions
-            for region in range(350):
+        # For connectivity features, iterate through edges
+        edge = 0
+        for i in range(350):
+            for j in range(i+1, 350):
                 res_curr = {
-                    "Dataset": dataset, "Repeat": np.floor(fold / n_folds),
+                    "Dataset": args.dataset, "Repeat": np.floor(fold / n_folds),
                     "Fold": fold % n_folds, "Target": target}
 
-                cols = [f"{feature}_{region}" for feature in features_arr]
-                if region < 300:
-                    cols = cols + [f"{feature}_{region}" for feature in features_surf]
-                train_x_region = train_x[cols]
-                test_x_regions = test_x[cols]
+                edge_opp = 350 * 350 - 1 - edge
+                cols = (
+                    [f"{feature}_{edge}" for feature in (features_sym + features_asym)]
+                    + [f"{feature}_{edge_opp}" for feature in features_asym])
+                cols_gmv = [f"s_acgmv_{edge}", f"s_acgmv_{edge_opp}"]
+                train_x_edge = [train_x[cols], x_ac["s_gmv"][cols_gmv].iloc[train_ind]]
+                test_x_edge = [test_x[cols], x_ac["s_gmv"][cols_gmv].iloc[test_ind]]
 
-                res_curr = res_curr | cca(train_x_region, train_y, test_x_regions, test_x)
-                results.append(pd.DataFrame(res_curr, index=[f"region_{region}"]))
+                # only include cortical features for edges between cortical regions
+                if i < 300 and j < 300:
+                    edge_opp_surf = 300 * 300 - 1 - edge
+                    for feature in ["cs", "ct"]:
+                        cols = [f"s_ac{feature}_{edge}", f"s_ac{feature}_{edge_opp_surf}"]
+                        train_x_edge.append(x_ac[f"s_{feature}"][cols].iloc[train_ind])
+                        test_x_edge.append(x_ac[f"s_{feature}"][cols].iloc[test_ind])
 
-            # For DTI features, iterate through the 50 parcels in the white matter atlas
-            for region in range(50):
-                res_curr = {
-                    "Dataset": dataset, "Repeat": np.floor(fold / n_folds),
-                    "Fold": fold % n_folds, "Target": target}
+                train_x_edge = pd.concat(train_x_edge, axis="columns")
+                test_x_edge = pd.concat(test_x_edge, axis="columns")
+                res_curr = res_curr | cca(train_x_edge, train_y, test_x_edge, test_y)
 
-                cols = [f"{feature}_{region}" for feature in features_d]
-                train_x_d = train_x[cols]
-                test_x_d = test_x[cols]
+                results.append(pd.DataFrame(res_curr, index=[f"edge_{edge}"]))
+                edge += 1
 
-                res_curr = res_curr | cca(train_x_d, train_y, test_x_d, test_x)
-                results.append(pd.DataFrame(res_curr, index=[f"dti_region_{region}"]))
+        # For region-wise features, iterate through brain regions
+        for region in range(350):
+            res_curr = {
+                "Dataset": args.dataset, "Repeat": np.floor(fold / n_folds),
+                "Fold": fold % n_folds, "Target": target}
+
+            cols = [f"{feature}_{region}" for feature in features_arr]
+            if region < 300:
+                cols = cols + [f"{feature}_{region}" for feature in features_surf]
+            train_x_region = train_x[cols]
+            test_x_regions = test_x[cols]
+
+            res_curr = res_curr | cca(train_x_region, train_y, test_x_regions, test_x)
+            results.append(pd.DataFrame(res_curr, index=[f"region_{region}"]))
+
+        # For DTI features, iterate through the 50 parcels in the white matter atlas
+        for region in range(50):
+            res_curr = {
+                "Dataset": args.dataset, "Repeat": np.floor(fold / n_folds),
+                "Fold": fold % n_folds, "Target": target}
+
+            cols = [f"{feature}_{region}" for feature in features_d]
+            train_x_d = train_x[cols]
+            test_x_d = test_x[cols]
+
+            res_curr = res_curr | cca(train_x_d, train_y, test_x_d, test_x)
+            results.append(pd.DataFrame(res_curr, index=[f"dti_region_{region}"]))
 
 # Save all resutls together
-pd.concat(results, axis="index").to_csv(Path(args.out_dir, "fusion_cca_results.csv"))
+pd.concat(results, axis="index").to_csv(Path(args.out_dir, f"fusion_cca_{args.dataset}.csv"))
